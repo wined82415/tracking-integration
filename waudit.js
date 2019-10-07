69ab413b40f52a75711b0b70ff79771845f0ee27
wined <wined@wabow.com>
2019-10-07 11:07:01 +0800

    feat: 雛形框架

---
+++ waudit.js
@@ -0,0 +1,4 @@
+"use strict";
+console.log(888);
+console.log(8789);
+//# sourceMappingURL=waudit.js.map
---
+++ webpack.config.js
@@ -0,0 +1,28 @@
+const path = require('path')
+
+module.exports = {
+  mode: 'production',
+  devtool: 'source-map',
+  entry: './src/index.ts',
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        use: 'ts-loader',
+        exclude: /node_modules/
+      },
+      {
+        enforce: 'pre',
+        test: /\.js$/,
+        loader: 'source-map-loader'
+      }
+    ]
+  },
+  resolve: {
+    extensions: ['.tsx', '.ts', '.js']
+  },
+  output: {
+    filename: 'waduit.js',
+    path: path.resolve(__dirname, 'dist')
+  }
+}
